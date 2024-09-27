c221607a7fe4f7abc868cecf66aefc898c9f5d43
saurabh singh <saurabhsingh73941@gmail.com>
2024-09-27 14:06:44 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -5,9 +5,30 @@ class Solution(object):
         :rtype: bool
         """
         pass
-    stack=[];
-    match_paren={')':'(', '}':'{',']':'['}
-    for char in s:
+    class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        matching_parentheses = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in matching_parentheses:  # It's a closing bracket
+                top_element = stack.pop() if stack else '#'
+                if matching_parentheses[char] != top_element:
+                    return False
+            else:
+                stack.append(char)  # It's an opening bracket
+        
+        return not stack  # Stack should be empty if valid
+
+# Example usage:
+sol = Solution()
+print(sol.isValid("()"))      # Output: True
+print(sol.isValid("()[]{}"))  # Output: True
+print(sol.isValid("(]"))      # Output: False
 
 
 
