e0738397268c3a3a4bc3bd3c3c8a7e0796c42198
saurabh singh <saurabhsingh73941@gmail.com>
2024-09-27 14:06:54 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,9 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-    class Solution(object):
-    def isValid(self, s):
+    
         """
         :type s: str
         :rtype: bool
