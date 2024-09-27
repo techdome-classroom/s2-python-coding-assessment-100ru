08f7253375caed4ad6c6ef9d381bb1123cdd427c
saurabh singh <saurabhsingh73941@gmail.com>
2024-09-27 14:39:07 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,12 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        class Solution(object):
-        def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
         roman_values = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
         }
