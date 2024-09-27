51f9562679f53fd2fd6c58185f22d95cbe4c3cb8
saurabh singh <saurabhsingh73941@gmail.com>
2024-09-27 14:28:08 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+         roman_values = {
+        'i': 1, 'v': 5, 'x': 10, 'l': 50, 'c': 100, 'd': 500, 'm': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    for char in reversed(s):
+        value = roman_values[char]
+        if value >= prev_value:
+            total += value
+        else:
+            total -= value
+        prev_value = value
+    
+    return total
+
+# Test example
+roman_number = "mcmxciv"  # Equivalent to 1994
+print(f"The integer value of the Roman numeral '{roman_number}' is {roman_to_int(roman_number)}.")
 
 
 
